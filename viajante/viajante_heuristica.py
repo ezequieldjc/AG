e9ceb1ee0f81763e84388eabd2491ea4d8d0604b
locolauty97 <ramos.lauty@gmail.com>
2017-09-20 11:30:43 -0300

    agregar iteracion en viajante para obtener recorrido de capitales

--- viajante/viajante_heuristica.py
+++ viajante/viajante_heuristica.py
@@ -1,17 +1,25 @@
 from nombres_capitales_viajante import capitales
 from tabla_distancias_viajante import distancias
+import operator
 
 visitadas = "00000000000000000000000"
 
 #obtengo las distancias a la capital indicada
-def getMatrizDistancias(indiceCiudad, tablaDistancias):
+def getMatrizDistancias(indiceCiudad):
+    global distancias
     if(int(indiceCiudad, 10) >= 1):
         matrizDistancias = distancias[indiceCiudad]
     for i in range(int(indiceCiudad) + 1, 24):
         matrizDistancias[str(i)] = distancias[str(i)][indiceCiudad]
-    return matrizDistancias
+    #devuelve una matriz de tuplas ordenadas por distancia de menor a mayor
+    return sorted(matrizDistancias.items(), key=operator.itemgetter(1))
 ##
 
+def cambiarVisitadas(indice, valor):
+    global visitadas
+    list1 = list(visitadas)
+    list1[indice] = valor
+    visitadas = ''.join(list1)
 
 
 # def getRecorrido(indiceCap, distancias, visitadas):
@@ -25,24 +33,31 @@ print "elija el numero de la capital de partida"
 
 #leo la entrada de consola
 numero_capital_partida = raw_input()
+numero_capital_actual = numero_capital_partida
 
 #obtengo el nombre de la capital e indice
 capital_partida = capitales[int(numero_capital_partida, 10) - 1]
 
-# print capital_partida
+print capital_partida
 
-# #obtengo las distancias a cada capital
-# if(int(numero_capital_partida, 10) >= 1):
-#     matriz_distancias = distancias[numero_capital_partida]
-# for i in range(int(numero_capital_partida) + 1, 24):
-#     matriz_distancias[str(i)] = distancias[str(i)][numero_capital_partida]
-##
-
-# print getMatrizDistancias(numero_capital_partida, distancias)
+cambiarVisitadas(int(numero_capital_partida, 10) - 1, "*")
 
 recorrido = []
-# iterintems() devuelve un objeto iterator que nos permite recorrer todos los items de
-# matriz_distancias para poder acceder al indice de ciudad y distancia
-# for i in matriz_distancias.iteritems():
-    # print i[1]
 
+for i in range(22):
+    dists = getMatrizDistancias(numero_capital_actual)
+    j = 0
+    encontrado = False
+    while(not encontrado):
+        if((visitadas[int(dists[j][0]) - 1] != "1") and (visitadas[int(dists[j][0]) - 1] != "*")):
+            encontrado = True
+            proximaCiudad = dists[j]
+            cambiarVisitadas(int(proximaCiudad[0]) - 1, "1")
+            numero_capital_actual = proximaCiudad[0]
+        else:
+            j = j + 1
+    recorrido.append(proximaCiudad)
+
+
+print visitadas
+print recorrido
