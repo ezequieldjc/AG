95bb144651472b034e7fe3e344756740f4e29ffd
Pablo Sgreccia <pablosgreccia@gmail.com>
2017-09-26 18:33:57 -0300

    listo pa

--- viajante/viajante_genetico.py
+++ viajante/viajante_genetico.py
@@ -8,25 +8,21 @@ import random
 def getMatrizDistancias(indiceCiudad):
 	global distancias
 	if(indiceCiudad >= 2):
-		matrizDistancias = distancias[indiceCiudad]
-	for i in range((indiceCiudad) + 1, 24):
-		matrizDistancias[str(i)] = distancias[str(i)][indiceCiudad]
+		matrizDistancias = distancias[str(indiceCiudad)]
+	for i in range(indiceCiudad + 1, 24):
+		matrizDistancias[str(i)] = distancias[str(i)][str(indiceCiudad)]
 	#devuelve una matriz de tuplas ordenadas por distancia de menor a mayor
 	return sorted(matrizDistancias.items(), key=operator.itemgetter(1))
 		
 #funcion objetivo
 def f_obj(ciudad):
-	total = 0
+	trayectoria = 0
 	for i in range(23):
 		rec = ciudad[i]
-		total = total + rec[1]
-	return total
+		trayectoria = trayectoria + rec[1]
+	return 1/float(trayectoria)
 	
 	
-#constantes
-numero_de_pruebas = 3
-
-
 #primer poblacion
 cromosomas = []
 for i in range(50):
@@ -42,33 +38,137 @@ for i in range(50):
 		ciudades = ''.join(list1)
 		cromosoma.append(num)
 	cromosomas.append(cromosoma)
+
 #print cromosomas
 #print ciudades
+print
+
+for q in range(200):
+	#Formo el recorrido y al final evaluo con la funcion objetivo
+	recorridos = []
+	evaluaciones = []
+	trayectos = []
+	for k in range(50):
+		cromosoma = cromosomas[k]
+		recorrido = []
+		#armo los recorridos (sin volver a la ciudad de origen)
+		for i in range(22):
+			if((cromosoma[i]) < (cromosoma[i+1])):
+				recorrido.append((str(cromosoma[i]), distancias[str(cromosoma[i+1])][str(cromosoma[i])]))
+			else:
+				recorrido.append((str(cromosoma[i]), distancias[str(cromosoma[i])][str(cromosoma[i+1])]))
+		# Vuelvo a la capital de origen
+		if((cromosoma[0]) < (cromosoma[22])):
+			recorrido.append( (str(cromosoma[0]), distancias[str(cromosoma[22])][str(cromosoma[0])]) )
+		else:
+			recorrido.append( (str(cromosoma[0]), distancias[str(cromosoma[0])][str(cromosoma[22])]) )
+		recorridos.append(recorrido)	
+		# obtener las evaluaciones de la funcion objetivo con cada cromosoma
+		evaluacion = f_obj(recorrido)
+		trayecto = int(math.pow(evaluacion, -1))
+		evaluaciones.append( evaluacion )
+		trayectos.append(trayecto)
+		
+		
+	
+	#print evaluaciones
+	if ((q == 0) or (q == 49) or (q == 99) or (q == 149) or (q == 199)):
+		print
+		print "----------------------"
+		print "Longitudes de trayectos en la iteracion " + str(q + 1) + ":"
+		print
+		print trayectos
+		print "Menor trayecto: " + str(min(trayectos)) 
+		menor = trayectos.index(min(trayectos))
+		print 
+		print "Recorrido: "
+		rec = recorridos[menor]
+		print rec
+		print "----------------------"
+
+	# ortener la suma de todas las evaluaciones 
+	totalEvaluaciones = sum(evaluaciones)
+
+	# obtener los fitness de cada cromosoma
+	losFitness = []
+	for i in range(50):
+		losFitness.append((evaluaciones[i]) / float(totalEvaluaciones))
+	#print losFitness
+
+	#hacemos la ruleta
+	arcosRuleta = []
+	for i in range(50):
+		arcosRuleta.append( int( round(losFitness[i] * 10000) ) )
+	#print arcosRuleta
+
+	ruleta = []
+	for i in range(len(arcosRuleta)):
+		for j in range(arcosRuleta[i]):
+			ruleta.append(i)
+	#print len(arcosRuleta)
+	#print len(ruleta)
+
+	#giramos la ruleta y obtenemos los pares de cromosomas
+	pares = []
+	for i in range(50):
+		pares.append(ruleta[int(random.randint(0,len(ruleta) - 1))])
+	#print pares
+
+	#crossover - probabilidad 0.75
+	poblacionNueva = []
+	j = 0
+	for i in range(25):
+		#se calcula la probabilidad de crossover
+		if(random.random() <= 0.95):
+			lugarDeCorte = random.randint(0,21)
+			hijo1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+			hijo2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+			padre1 = cromosomas[pares[j]]
+			padre2 = cromosomas[pares[j+1]]
+			#crossover ciclico
+			primerNum = padre1[0] 
+			hijo1[0] = primerNum 
+			segundoNum = padre2[0] 
+			hijo2[0] = segundoNum
+			while (segundoNum != primerNum):
+				indice = padre1.index(segundoNum)
+				hijo1[indice] = segundoNum
+				hijo2[indice] = padre2[indice]
+				segundoNum = padre2[indice]
+			for m in range(23):
+				if (hijo1[m] == 0):
+					hijo1[m] = padre2[m]
+					hijo2[m] = padre1[m]
+
+			poblacionNueva.append(hijo1)
+			poblacionNueva.append(hijo2)
+			j = j + 2
+		else:
+			padre1 = cromosomas[pares[j]]
+			padre2 = cromosomas[pares[j+1]]
+			poblacionNueva.append(padre1)
+			poblacionNueva.append(padre2)
+			j = j + 2
+	# for r in range(50):
+		# print poblacionNueva[r]
+	#print len(poblacionNueva)
+		
+	#mutacion - 0.05
+	#se calcula la probabilidad de mutacion
+	for i in range(50):
+		if(random.random() <= 0.05):
+			genAleatorio1 = random.randint(0,22)
+			genAleatorio2 = random.randint(0,22)
+			
+			num = poblacionNueva[i][genAleatorio1]
+			poblacionNueva[i][genAleatorio1] = poblacionNueva[i][genAleatorio2]
+			poblacionNueva[i][genAleatorio2] = num
+
+	cromosomas = []
+	for i in range(50):
+		cromosomas.append(poblacionNueva[i])
 
 
-#Formo el recorrido y al final evaluo con la funcion objetivo
-recorridos = []
-evaluaciones = []
-for k in range(50):
-	cromosoma = cromosomas[k]
-	numero_capital_actual = cromosoma[0]
-	recorrido = []
-	for i in range(22):
-		dists = getMatrizDistancias(numero_capital_actual)
-		proximaCiudad = dists[cromosoma[i+1]]
-		numero_capital_actual = proximaCiudad[0]
-		recorrido.append(proximaCiudad)
-	#Vuelvo a la capital de origen
-	if(int(cromosoma[i+1]) < int(cromosoma[22])):
-		recorrido.append( (numero_capital_partida, distancias[numero_capital_actual][numero_capital_partida]) )
-	else:
-		recorrido.append( (numero_capital_partida, distancias[numero_capital_partida][numero_capital_actual]) )
-	recorridos.append(recorrido)	
-	#obtener las evaluaciones de la funcion objetivo con cada cromosoma
-	evaluacion = f_obj(recorrido)
-	evaluaciones.append( evaluacion )
-
-print evaluaciones
 
 
 
@@ -84,3 +184,14 @@ print evaluaciones
 
 
 
+
+
+
+
+
+	
+	
+	
+	
+	
+	
