5cb8857699cc17b035a668ea389951da0465adb6
locolauty97 <ramos.lauty@gmail.com>
2017-09-26 20:29:15 -0300

    update de menu viajante

--- viajante/viajante.py
+++ viajante/viajante.py
@@ -3,6 +3,11 @@ from tabla_distancias_viajante import distancias
 print "elija metodo de resolucion: 1-heuristica, 2-genetico"
 seleccion = raw_input()
 if(seleccion == "1"):
-    import viajante_heuristica
+    print "elegir punto de partida? (1)"
+    seleccion = raw_input()
+    if(seleccion == "1"):
+        import viajante_heuristica
+    else:
+        import viajante_con_heuristica_2
 else:
     import viajante_genetico
