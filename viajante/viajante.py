a1ab9016a38bb3e888914bada88fe697f234e887
locolauty97 <ramos.lauty@gmail.com>
2017-09-26 20:25:26 -0300

    agregar menu de opciones a viajante

--- viajante/viajante.py
+++ viajante/viajante.py
@@ -1,3 +1,8 @@
 from tabla_distancias_viajante import distancias
-
 #aca va a estar el menu de opciones
+print "elija metodo de resolucion: 1-heuristica, 2-genetico"
+seleccion = raw_input()
+if(seleccion == "1"):
+    import viajante_heuristica
+else:
+    import viajante_genetico
